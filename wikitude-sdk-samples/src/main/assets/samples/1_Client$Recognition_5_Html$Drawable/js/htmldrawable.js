f069de38ea1690ed83fc0a6e9a1152d1ace4ee13
Jeremy Chen <jeremychen0127@gmail.com>
2016-10-15 23:51:12 -0400

    Added news head lines

--- wikitude-sdk-samples/src/main/assets/samples/1_Client$Recognition_5_Html$Drawable/js/htmldrawable.js
+++ wikitude-sdk-samples/src/main/assets/samples/1_Client$Recognition_5_Html$Drawable/js/htmldrawable.js
@@ -13,11 +13,29 @@ var World = {
 
 		var apple_price_video = new AR.VideoDrawable("assets/apple_price.mp4", 0.40, {
             offsetY: -0.6,
+            scale: 1.2
+        });
+
+        var apple_head = new AR.HtmlDrawable({
+            uri: "assets/apple_head.html"
+        }, 0.25, {
+            viewportWidth: 720,
+            viewportHeight: 50,
+            scale: 10,
+            offsetX: 0.12,
+            offsetY: 0.55,
+            horizontalAnchor: AR.CONST.HORIZONTAL_ANCHOR.RIGHT,
+            verticalAnchor: AR.CONST.VERTICAL_ANCHOR.TOP,
+            clickThroughEnabled: true,
+            allowDocumentLocationChanges: false,
+            onDocumentLocationChanged: function onDocumentLocationChangedFn(uri) {
+                AR.context.openInBrowser(uri);
+            }
         });
 
         var apple = new AR.Trackable2DObject(this.tracker, "apple", {
             drawables: {
-                cam: [apple_price_video]
+                cam: [apple_price_video, apple_head]
             },
             onEnterFieldOfVision: function onEnterFieldOfVisionFn() {
                 apple_price_video.play(-1);
@@ -26,11 +44,29 @@ var World = {
 
         var yelp_price_video = new AR.VideoDrawable("assets/yelp_price.mp4", 0.40, {
             offsetY: -0.6,
+            scale: 1.2
+        });
+
+        var yelp_head = new AR.HtmlDrawable({
+            uri: "assets/yelp_head.html"
+        }, 0.25, {
+            viewportWidth: 720,
+            viewportHeight: 50,
+            scale: 10,
+            offsetX: 0.12,
+            offsetY: 0.55,
+            horizontalAnchor: AR.CONST.HORIZONTAL_ANCHOR.RIGHT,
+            verticalAnchor: AR.CONST.VERTICAL_ANCHOR.TOP,
+            clickThroughEnabled: true,
+            allowDocumentLocationChanges: false,
+            onDocumentLocationChanged: function onDocumentLocationChangedFn(uri) {
+                AR.context.openInBrowser(uri);
+            }
         });
 
         var yelp = new AR.Trackable2DObject(this.tracker, "yelp", {
             drawables: {
-                cam: [yelp_price_video]
+                cam: [yelp_price_video, yelp_head]
             },
             onEnterFieldOfVision: function onEnterFieldOfVisionFn() {
                 yelp_price_video.play(-1);
@@ -39,11 +75,29 @@ var World = {
 
         var walmart_price_video = new AR.VideoDrawable("assets/walmart_price.mp4", 0.40, {
             offsetY: -0.6,
+            scale: 1.2
+        });
+
+        var walmart_head = new AR.HtmlDrawable({
+            uri: "assets/walmart_head.html"
+        }, 0.25, {
+            viewportWidth: 720,
+            viewportHeight: 50,
+            scale: 10,
+            offsetX: 0.12,
+            offsetY: 0.55,
+            horizontalAnchor: AR.CONST.HORIZONTAL_ANCHOR.RIGHT,
+            verticalAnchor: AR.CONST.VERTICAL_ANCHOR.TOP,
+            clickThroughEnabled: true,
+            allowDocumentLocationChanges: false,
+            onDocumentLocationChanged: function onDocumentLocationChangedFn(uri) {
+                AR.context.openInBrowser(uri);
+            }
         });
 
         var walmart = new AR.Trackable2DObject(this.tracker, "walmart", {
             drawables: {
-                cam: [walmart_price_video]
+                cam: [walmart_price_video, walmart_head]
             },
             onEnterFieldOfVision: function onEnterFieldOfVisionFn() {
                 walmart_price_video.play(-1);
@@ -52,11 +106,29 @@ var World = {
 
         var google_price_video = new AR.VideoDrawable("assets/google_price.mp4", 0.40, {
             offsetY: -0.6,
+            scale: 1.2
+        });
+
+        var google_head = new AR.HtmlDrawable({
+            uri: "assets/google_head.html"
+        }, 0.25, {
+            viewportWidth: 720,
+            viewportHeight: 50,
+            scale: 10,
+            offsetX: 0.12,
+            offsetY: 0.55,
+            horizontalAnchor: AR.CONST.HORIZONTAL_ANCHOR.RIGHT,
+            verticalAnchor: AR.CONST.VERTICAL_ANCHOR.TOP,
+            clickThroughEnabled: true,
+            allowDocumentLocationChanges: false,
+            onDocumentLocationChanged: function onDocumentLocationChangedFn(uri) {
+                AR.context.openInBrowser(uri);
+            }
         });
 
         var google = new AR.Trackable2DObject(this.tracker, "google", {
             drawables: {
-                cam: [google_price_video]
+                cam: [google_price_video, google_head]
             },
             onEnterFieldOfVision: function onEnterFieldOfVisionFn() {
                 google_price_video.play(-1);
@@ -65,11 +137,29 @@ var World = {
 
         var ae_price_video = new AR.VideoDrawable("assets/ae_price.mp4", 0.40, {
             offsetY: -0.6,
+            scale: 1.2
+        });
+
+        var ae_head = new AR.HtmlDrawable({
+            uri: "assets/ae_head.html"
+        }, 0.25, {
+            viewportWidth: 720,
+            viewportHeight: 50,
+            scale: 10,
+            offsetX: 0.12,
+            offsetY: 0.55,
+            horizontalAnchor: AR.CONST.HORIZONTAL_ANCHOR.RIGHT,
+            verticalAnchor: AR.CONST.VERTICAL_ANCHOR.TOP,
+            clickThroughEnabled: true,
+            allowDocumentLocationChanges: false,
+            onDocumentLocationChanged: function onDocumentLocationChangedFn(uri) {
+                AR.context.openInBrowser(uri);
+            }
         });
 
         var ae = new AR.Trackable2DObject(this.tracker, "ae", {
             drawables: {
-                cam: [ae_price_video]
+                cam: [ae_price_video, ae_head]
             },
             onEnterFieldOfVision: function onEnterFieldOfVisionFn() {
                 ae_price_video.play(-1);
